aea9aa3c56945126ea0d768c9ca0178ae6fbb2b2
crystalhwaang <chwang610@gmail.com>
2023-03-01 22:55:21 -0700

    Add project files.

---
+++ Mission09_clhwang/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission09_clhwang.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission09_clhwang.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IBookstoreRepository repo;
+        public HomeController (IBookstoreRepository temp)
+        {
+            repo = temp;
+        }
+        public IActionResult Index(int pageNum = 1)
+        {
+            int pageSize = 5;
+
+            var blah = repo.Books
+                .OrderBy(b => b.Title)
+                .Skip(pageNum * pageSize)
+                .Take(pageSize);
+            return View(blah);
+        }
+    }
+}
