e9290f68a69eb8813495655df0da897a4e16ead3
crystalhwaang <chwang610@gmail.com>
2023-03-14 13:22:23 -0600

    final

---
+++ Mission09_clhwang/Components/BasketSummaryViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission09_clhwang.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission09_clhwang.Components
+{
+    public class BasketSummaryViewComponent : ViewComponent
+    {
+        private Basket basket;
+        public BasketSummaryViewComponent (Basket basketService)
+        {
+            basket = basketService;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(basket);
+        }
+    }
+}
